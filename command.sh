7303a58759484df83499495b6496ab0cb96c2a3c
tauhidbs23 <tauhidul.islam@brainstation23.com>
2021-12-08 23:12:54 +0600

    git config branch.autosetuprebase always

---
+++ command.sh
@@ -0,0 +1,8 @@
+git config --global branch.autosetuprebase always
+branch.<branchname>.rebase=true
+git config branch.<branchname>.rebase true
+
+---------------------
+git config branch.autosetuprebase always
+git config branch.<main>.rebase=true
+git config branch.<main>.rebase true
